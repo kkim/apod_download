394a39e4108c888fe374d9f4ff3de169d35fff1b
Kio Kim SB <kiokim@gmail.com>
2015-07-04 08:06:44 -0700

    No need for temporary html file, using urllib2 and re

--- getapod.py
+++ getapod.py
@@ -2,41 +2,27 @@
 
 import sys,os
 import urllib
+import urllib2
+import re
 
 WALL_PAPER_FILE_NAME = './apod.jpg'
+APOD_URL = "http://antwrp.gsfc.nasa.gov/apod/astropix.html"
 
-def get_img_url():
-    return os.popen("grep href..image.*[jpg\|png\|gif] astropix.html").readline().rstrip('\n')
+def get_image_file_name_from_url(url):
+    try:
+        apodhtml = urllib2.urlopen(url).read()
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        print "Internet connection problem?"
+        sys.exit()
 
-def get_vimeo_url():
-    return os.popen("grep player.vimeo.com astropix.html").readline().rstrip('\n')
+    matches = re.search('href..(image.*[jpg\|png\|gif])',apodhtml)
+    image_file_name = matches.group(1)
+    #print 'matches', image_file_name
+    return image_file_name
 
-def get_youtube_url():
-    return os.popen("grep youtube astropix.html").readline().rstrip('\n')
 
-def get_image_file_name_from_url(apodimg):
-    return apodimg.split("\"")[1];
-
-def get_vimeo_id_from_url(apodimg):
-    return apodvimeo.split("\"")[1].split("/")[-1] # last element
-
-if os.path.isfile("astropix.html"): os.remove("astropix.html")
-
-try:
-    urllib.urlretrieve("http://antwrp.gsfc.nasa.gov/apod/astropix.html","./astropix.html")
-except IOError as e:
-    print "I/O error({0}): {1}".format(e.errno, e.strerror)
-    print "Internet connection problem?"
-    sys.exit()
-    
-apodimg = get_img_url()
-apodvimeo = get_vimeo_url()
-apodyoutube = get_youtube_url()
-
-if len(apodimg) > 0 :
-    imgfile = get_image_file_name_from_url(apodimg)
-    urllib.urlretrieve("http://antwrp.gsfc.nasa.gov/apod/"+imgfile,WALL_PAPER_FILE_NAME)
-elif len(apodvimeo) > 0 :
-    vimeoid = get_vimeo_id_from_url(apodvimeo)
+imgfile = get_image_file_name_from_url(APOD_URL)
+urllib.urlretrieve("http://antwrp.gsfc.nasa.gov/apod/"+imgfile,WALL_PAPER_FILE_NAME)
     
 
