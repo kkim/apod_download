64f3a4775173a838e42a2fcbef2bbaaa9b718969
Kio Kim SB <kiokim@gmail.com>
2015-07-22 01:16:35 -0700

    If youtube clip is posted, download a thumbnail

--- getapod.py
+++ getapod.py
@@ -17,12 +17,29 @@ def get_image_file_name_from_url(url):
         sys.exit()
 
     matches = re.search('href..(image.*[jpg\|png\|gif])',apodhtml)
-    image_file_name = matches.group(1)
+    image_file_name = matches and matches.group(1)
     #print 'matches', image_file_name
     return image_file_name
 
+def get_youtube_id(url):
+    try:
+        apodhtml = urllib2.urlopen(url).read()
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+        print "Internet connection problem?"
+        sys.exit()
+
+    matches = re.search('youtube.com/embed/([0-9a-zA-Z]+)?',apodhtml)
+    youtube_id = matches and matches.group(1)
+    print 'matches', youtube_id
+    return youtube_id
 
 imgfile = get_image_file_name_from_url(APOD_URL)
-urllib.urlretrieve("http://antwrp.gsfc.nasa.gov/apod/"+imgfile,WALL_PAPER_FILE_NAME)
-    
-
+if imgfile:
+    urllib.urlretrieve("http://antwrp.gsfc.nasa.gov/apod/"+imgfile,WALL_PAPER_FILE_NAME)
+else:
+    print 'No image file found'    
+    youtube_id = get_youtube_id(APOD_URL)
+    if youtube_id:
+        urllib.urlretrieve("https://img.youtube.com/vi/"+youtube_id+'/0.jpg',WALL_PAPER_FILE_NAME)
+        
